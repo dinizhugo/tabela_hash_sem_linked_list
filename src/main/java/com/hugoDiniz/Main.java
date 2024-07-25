e3069803c24b70635914cc7f58f6bb1eef3e9f6a
Hugo-Diniz <hugodiniztech01@gmail.com>
2024-07-25 12:33:12 -0300

    testes

--- src/main/java/com/hugoDiniz/Main.java
+++ src/main/java/com/hugoDiniz/Main.java
@@ -1,17 +1,16 @@
 package com.hugoDiniz;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.hugoDiniz.domain.tabela.TabelaHash;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        TabelaHash tabelaHash = new TabelaHash(5);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        tabelaHash.insertAluno("0", "hugo");
+        tabelaHash.insertAluno("1", "hugo");
+        tabelaHash.insertAluno("2", "hugo");
+        tabelaHash.insertAluno("3", "hugo");
+
+        System.out.println(tabelaHash.getAllAlunos());
     }
 }
