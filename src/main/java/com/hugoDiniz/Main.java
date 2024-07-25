dd0c78d159a33dd65fb9a38cf931641f9327e538
Hugo-Diniz <hugodiniztech01@gmail.com>
2024-07-25 12:51:11 -0300

    testes

--- src/main/java/com/hugoDiniz/Main.java
+++ src/main/java/com/hugoDiniz/Main.java
@@ -6,11 +6,31 @@ public class Main {
     public static void main(String[] args) {
         TabelaHash tabelaHash = new TabelaHash(5);
 
-        tabelaHash.insertAluno("0", "hugo");
-        tabelaHash.insertAluno("1", "hugo");
-        tabelaHash.insertAluno("2", "hugo");
-        tabelaHash.insertAluno("3", "hugo");
+        System.out.println("Inserindo alunos:");
+        tabelaHash.insertAluno("0", "a");
+        tabelaHash.insertAluno("1", "b");
+        tabelaHash.insertAluno("2", "c");
+        tabelaHash.insertAluno("3", "d");
+        System.out.println("size: " + tabelaHash.getSize());
 
         System.out.println(tabelaHash.getAllAlunos());
+        System.out.println("============================");
+        System.out.println("Aumentando a tabela com mais alunos:");
+        tabelaHash.insertAluno("3", "e");
+        tabelaHash.insertAluno("4", "f");
+        tabelaHash.insertAluno("5", "g");
+        tabelaHash.insertAluno("6", "Cleison");
+        System.out.println(tabelaHash.getAllAlunos());
+        System.out.println("size: " + tabelaHash.getSize());
+
+        System.out.println("============================");
+        System.out.println("Removendo aluno Clesion:");
+        tabelaHash.removeAluno("6");
+        System.out.println(tabelaHash.getAllAlunos());
+        System.out.println("size: " + tabelaHash.getSize());
+
+
+
+
     }
 }
